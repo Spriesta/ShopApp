332726912f91361c32132c1f961cf35a300b73c7
Spriesta <kahvegozlum566@gmail.com>
2021-09-10 16:22:56 +0300

    firstDay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
 import { Component } from '@angular/core';
+import { products } from "./products";  //karışıklık olmasın diye products.ts oluşturdum ve buraya import edip instance oluşturup rahatça ulaştım
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Shop';
+
+  products = products ;
+
+  basket:Product[] =[];
+
+
+
+
+  getTotal() :string{
+    let total= 0 ;
+
+    for(let item of this.basket){
+      total += item.price*item.quantity;
+    }
+    return total.toFixed(2)+ ' TL'
+  }
+
+
+
+  decreaseAmount(){
+    // ürün miktarı sıfır ise pasif veya azalt
+
+  }
+
+
+
+  increaseAmount(product :Product):void{
+    // sepette yoksa ekliyoruz
+    if (!this.basket.includes(product)) {
+      this.basket.push(product);
+    }
+
+
+    // ürün miktarını arttırıyoruz
+    product.quantity++ ;
+  }
+
+
 }
+
+type Product =
+  {
+    id : number,
+    name : string,
+    photoPath : string,
+    price : number,
+    unit : string,
+    quantity : number
+  }
+
+
---
+++ src/app/products.ts
@@ -0,0 +1,52 @@
+export const products =
+ [
+  {
+    id : 1,
+    name : 'Domates',
+    photoPath : 'https://i.resmim.net/VRBLy.jpg',
+    price : 3.99,
+    unit : 'Kg',
+    quantity : 0
+  },
+  {
+    id : 2,
+    name : 'Çipura',
+    photoPath : 'https://i.resmim.net/VRYmD.jpg',
+    price : 14,
+    unit : 'Adet',
+    quantity : 0
+  },
+  {
+    id : 3,
+    name : 'Sek Süt 1L',
+    photoPath : 'https://i.resmim.net/VRovZ.jpg',
+    price : 8.2,
+    unit : 'Adet',
+    quantity : 0
+  },
+  {
+    id : 4,
+    name : 'Pastavilla Burgu Makarna 500G',
+    photoPath : 'https://i.resmim.net/VR80n.jpg',
+    price : 5.25,
+    unit : 'Adet',
+    quantity : 0
+  },
+  {
+    id : 5,
+    name : 'Coca-Cola Zero Sugar 1 L',
+    photoPath : 'https://i.resmim.net/VRbYI.png',
+    price : 4.95,
+    unit : 'Adet',
+    quantity : 0
+  },
+  {
+    id : 6,
+    name : 'Solo Havlu 12\'Li',
+    photoPath : 'https://i.resmim.net/VRe6b.jpg',
+    price : 43.5,
+    unit : 'Adet',
+    quantity : 0
+  },
+
+]
