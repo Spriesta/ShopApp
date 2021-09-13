8f4de6f870f91e16dc300d5537dfc747ad85970c
Spriesta <kahvegozlum566@gmail.com>
2021-09-13 12:35:55 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'products',component:ProductsComponent},
+  {path:'orders',component:OrdersComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
